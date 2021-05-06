c69da1dd6016a587aeb760de9a3235ea49d9933a
Andrei Jiroh Eugenio Halili <andreijiroh@madebythepins.tk>
2021-05-06 20:58:41 +0800

    ci: Update script to include some debugging tools
    Signed-off-by: Andrei Jiroh Eugenio Halili <andreijiroh@madebythepins.tk>

--- scripts/index-bootstrapper.sh
+++ scripts/index-bootstrapper.sh
@@ -2,13 +2,19 @@
 download_script(){
   echo "--------------------------------------------------"
   echo "Downloading bootstrapper for $1 from $2..."
-  wget --verbose https://raw.githubusercontent.com/"$2"/"$3"/"${4:toolkits/virtual-machines/bootstrapper}" -O src/scripts/"$1"
+  echo "URL was https://raw.githubusercontent.com/$2/$3/$4"
+  wget --verbose https://raw.githubusercontent.com/$2/$3/$4 -O src/scripts/"$1"
   if [[ $? != "0" ]]; then
     echo "Failed to download bootstrapper link for $1. Error code from Wget executation  was $?"
     echo "If the repository has been renamed or moved, please go to the following link to"
     echo "update it in the registry:"
     echo
     echo "   https://cdrs-deploy.repohubdev.tk/register/submit-update/?template-slug=$1&isWarning=1"
+    echo
+    echo "Otherwise, report CI errors using this link instead:"
+    echo
+    echo "   https://cdrs-deploy.repohubdev.tk/source/+fileabug"
+    echo
   else
     echo "Success!"
   fi
@@ -20,7 +26,7 @@ download_script "deploy-code-server-upstream" "cdr/deploy-code-server" main "dep
 sleep 3
 
 # https://github.com/code-server-boilerplates/starter-pack
-download_script "example-project" "code-server-boilerplates/starter-pack" main
+download_script "example-project" "code-server-boilerplates/starter-pack" main toolkits/virtual-machines/bootstrapper
 sleep 3
 
 ### template - start ###
